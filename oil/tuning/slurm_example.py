b42022525e5b2b50edc626034881cc9e2f409abb
Marc Anton Finzi <mfinzi@hmc.edu>
2019-01-16 02:38:01 -0500

    more examples for slurm executor

--- oil/tuning/slurm_example.py
+++ oil/tuning/slurm_example.py
@@ -7,7 +7,11 @@ def square(n):
     return n * n
 def hostinfo():
     return subprocess.check_output('uname -a', shell=True)
-
+def gpustat():
+    return subprocess.check_output('gpustat', shell=True)
+def cpu_count():
+    import multiprocessing
+    return multiprocessing.cpu_count()
 def example_1():
     """Square some numbers on remote hosts!
     """
@@ -21,10 +25,10 @@ def example_2():
     our jobs.
     """
     with SlurmPoolExecutor(max_workers=5) as executor:
-        futures = [executor.submit(hostinfo) for n in range(15)]
+        futures = [executor.submit(gpustat) for n in range(5)]
         print('Some cluster nodes:')
         for future in concurrent.futures.as_completed(futures):
-            print((future.result().strip()))
+            print(future.result())
 
 # def example_3():
 #     """Demonstrates the use of the map() convenience function.
@@ -33,6 +37,6 @@ def example_2():
 #     print((list(cfut.map(exc, square, [5, 7, 11]))))
 
 if __name__ == '__main__':
-    example_1()
-    #example_2()
+    #example_1()
+    example_2()
     #example_3()
