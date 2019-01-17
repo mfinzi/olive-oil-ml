491a8503a9e238e9a627524ad469c57aa3d788a9
Marc Finzi <mfinzi@hmc.edu>
2019-01-16 23:21:10 -0500

    added study documentation and corrected savepath

--- oil/tuning/study.py
+++ oil/tuning/study.py
@@ -15,16 +15,20 @@ def slurm_available():
 
 DEFAULT_SLURM_SETTINGS = {
     'N':1,
-    'c':1,
-    'mem':24000,
+    'c':1, # Number of cores per node
+    'mem':24000, # mem specifies the maximum virtual memory which includes max GPU ram
     'time': '24:00:00',
     'partition':'default_gpu',
     'gres':'gpu:1080ti:1',
 }
 
 class Study(object):
+    """ The study object allows hyperparameter search experimentation, with
+        automatic trial parrallelization via Slurm if available. The trial configs
+        that are run are stored in the dataframe self.configs, 
+        and trial outcomes in self.outcomes. """
     def __init__(self, perform_trial, config_spec,
-                        slurm=True, slurm_cfg={}, study_name=None):
+                    slurm_cfg={}, study_name=None):
         self.perform_trial = perform_trial
         self.config_spec = config_spec
         slurm_settings = {**DEFAULT_SLURM_SETTINGS,**slurm_cfg}
@@ -33,13 +37,18 @@ class Study(object):
         self.configs = pd.DataFrame()
         self.outcomes = pd.DataFrame()
         self.name = study_name or __file__[:-3]
+
     def flat_configs(self):
+        """ Return a dataframe where rows are flattened versions of self.configs"""
         flat_cfgs = pd.DataFrame()
         for row in self.configs.apply(flatten_dict,axis=1):
             flat_cfgs.append(row,ignore_index=True)
         return flat_cfgs
 
     def run(self, num_trials, max_workers=10, new_config_spec=None):
+        """ runs the study with num_trials and max_workers slurm nodes
+            trials are executed in parallel by the slurm nodes, study object
+            is updated and saved as results come in """
         if new_config_spec: self.config_spec=new_config_spec
         with self.Executor(max_workers) as executor:
             futures = [executor.submit(self.perform_trial,
@@ -48,10 +57,10 @@ class Study(object):
                 cfg, outcome = future.result()
                 self.configs.append(cfg,ignore_index=True)
                 self.outcomes.append(outcome,ignore_index=True)
-                torch.save(self,"study.s",pickle_module=dill)
+                torch.save(self,self.name+'.s',pickle_module=dill)
 
 def train_trial(make_trainer):
-    """ a common trainer trial use case """
+    """ a common trainer trial use case: make_trainer, train, return cfg and emas"""
     def _perform_trial(cfg,i):
         cfg['trainer_config']['log_dir'] += 'trial{}/'.format(i)
         trainer = make_trainer(cfg)
