b67f8faddd1782df385f9f26f1e6869702389bf7
Marc Anton Finzi <mfinzi@hmc.edu>
2019-01-16 22:17:16 -0500

    fixed cutout script

--- experiments/cutout.py
+++ experiments/cutout.py
@@ -9,24 +9,25 @@ from oil.datasetup.dataloaders import getLabLoader
 from oil.datasetup.augLayers import RandomErasing,Cutout
 from oil.architectures.img_classifiers.networkparts import layer13
 from oil.utils.utils import cosLr, loader_to
+from oil.tuning.configGenerator import uniform,logUniform,sample_config
 
 train_epochs = 100
 DataSet = CIFAR100
 net_config =        {'numClasses':DataSet.num_classes}
-loader_config =     {'amnt_dev':5000,'lab_BS':50,'dataseed':0,'num_workers':4}
+loader_config =     {'amnt_dev':5000,'lab_BS':50}
 opt_config =        {'lr':.1, 'momentum':.9, 'weight_decay':1e-4, 'nesterov':True}
 sched_config =      {'cycle_length':train_epochs}
+cutout_config = {'p':uniform(.3,1)(0),'af':logUniform(.1,.5)(0),'ar':logUniform(1,3)(0)}
 trainer_config =    {}
-all_hypers = {**net_config,**loader_config,**opt_config,**sched_config,**trainer_config}
 
-trainer_config['log_dir'] = os.path.expanduser('~/tb-experiments/c100cutoutproper/')
+trainer_config['log_dir'] = os.path.expanduser('~/tb-experiments/c100cutout2/')
 
 def makeTrainer():
-    trainset = DataSet('~/datasets/{}/'.format(DataSet.__name__))
+    trainset = DataSet('~/datasets/{}/'.format(DataSet))
     device = torch.device('cuda')
     fullCNN = nn.Sequential(
         trainset.default_aug_layers(),
-        Cutout(),
+        RandomErasing(**cutout_config),
         layer13(**net_config).to(device)
     )
     
@@ -36,7 +37,7 @@ def makeTrainer():
 
     opt_constr = lambda params: optim.SGD(params, **opt_config)
     lr_sched = cosLr(**sched_config)
-    return Classifier(fullCNN,dataloaders,opt_constr,lr_sched,**trainer_config,tracked_hypers=all_hypers)
+    return Classifier(fullCNN,dataloaders,opt_constr,lr_sched,**trainer_config)
 
 trainer = makeTrainer()
 trainer.train(train_epochs)
