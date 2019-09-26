c40dfc7a92a1b159fa779757a9351e9b2e14ff87
Marc Finzi <mfinzi@hmc.edu>
2019-09-26 14:04:55 -0700

    Adjusted mse loss in pi model so as to average over minibatch rather than sum

--- oil/utils/losses.py
+++ oil/utils/losses.py
@@ -9,14 +9,14 @@ def softmax_mse_loss(input_logits, target_logits):
     """Takes softmax on both sides and returns MSE loss
     Note:
     - Returns the sum over all examples. Divide by the batch size afterwards
-      if you want the mean.
+      if you want the mean. (not true anymore)
     - Sends gradients to inputs but not the targets.
     """
     assert input_logits.size() == target_logits.size()
     input_softmax = F.softmax(input_logits, dim=1)
     target_softmax = F.softmax(target_logits, dim=1)
     num_classes = input_logits.size()[1]
-    return F.mse_loss(input_softmax, target_softmax, size_average=False) / num_classes
+    return F.mse_loss(input_softmax, target_softmax) / num_classes
 
 
 def softmax_kl_loss(input_logits, target_logits):
