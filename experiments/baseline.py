ba30766c40f892aff9ab364dc0503904f6f4673d
Marc Finzi <mfinzi@hmc.edu>
2019-11-01 13:01:46 -0700

    house cleaning

--- experiments/baseline.py
+++ experiments/baseline.py
@@ -2,8 +2,9 @@
 from oil.model_trainers.classifier import Classifier,simpleClassifierTrial,base_cfg
 from oil.tuning.args import argupdated_config
 import os
-
-config_spec = {**base_cfg,**{'trainer_config':{'log_dir':os.path.expanduser('~/tb-experiments/deconv_bs_large/')}}}
-Trial = simpleClassifierTrial
-cutout_study = Study(Trial,argupdated_config(config_spec))
-cutout_study.run()
+from oil.tuning.study import Study
+if __name__=="__main__":
+    config_spec = {**base_cfg,**{'trainer_config':{'log_dir':os.path.expanduser('~/tb-experiments/modelnet_hyper/')}}}
+    Trial = simpleClassifierTrial
+    cutout_study = Study(Trial,argupdated_config(config_spec),study_name="point3d",base_log_dir=os.path.expanduser('~/tb-experiments/modelnet_hyper/'))
+    cutout_study.run()
