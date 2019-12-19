81dc77869b47f525cbe456f16a58ee1a69e61ba9
Marc Finzi <mfinzi@hmc.edu>
2019-12-19 15:44:24 -0500

    fix

--- oil/tuning/args.py
+++ oil/tuning/args.py
@@ -22,7 +22,7 @@ def argupdated_config(cfg,parser=None, namespace=None):
         clobbered_name_mapping[short_field_name] = field
     parser.add_argument("--local_rank",type=int) # so that distributed will work #TODO: sort this out properly
     args = parser.parse_args()
-    if namespace is not None: from namespace import *
+    #if namespace is not None: from namespace import *
     for short_argname, argvalue in vars(args).items():
         if isinstance(argvalue,str):
             try: argvalue = eval(argvalue) # Try to evaluate the strings
