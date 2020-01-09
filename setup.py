d89d62b0725840b37b382ba87ac045d753d2d8af
mfinzi <mfinzi@hmc.edu>
2020-01-09 10:00:44 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ setup(name="snake-oil-ml",
       author='Marc Finzi',
       author_email='maf388@cornell.edu',
       license='MIT',
-      install_requires=['torch','torchvision','pandas','numpy','matplotlib','dill','sklearn',
-      'tqdm','torchcontrib'],#,'torch-scatter','torch-sparse','torch-cluster','torch-geometric','torch-spline-conv'],
+      install_requires=['torch>=1.3','torchvision','pandas','numpy','matplotlib','dill','sklearn',
+      'tqdm>=4.40','torchcontrib'],#,'torch-scatter','torch-sparse','torch-cluster','torch-geometric','torch-spline-conv'],
       packages=find_packages(),#["oil",],#find_packages()
       long_description=open('README.md').read(),
 )
